7fcd4441c323304f87973bb2d638e94f8af5c0bc
dofferni <dofferni@quoter.com>
2022-09-11 02:37:03 -0700

    Create http method done, making the stores map accessible to the Create method

--- mocks/create_store.go
+++ mocks/create_store.go
@@ -1,19 +1,14 @@
 package mocks
 
 import (
-	"fmt"
-
 	"github.com/offerni/graphqllearning"
 )
 
 func CreateStore(opts CreateOpts) (*graphqllearning.Store, error) {
 	id := graphqllearning.RandomString(10)
 
-	if stores[id].ID == id {
-		return nil, fmt.Errorf("Store already exists")
-	}
-
 	stores[id] = &graphqllearning.Store{
+		ID:   id,
 		Name: opts.Name,
 	}
 
--- mocks/find_store_by_id.go
+++ mocks/find_store_by_id.go
@@ -7,29 +7,23 @@ import (
 )
 
 // not ideal but just for testing ;)
-var (
-	stores map[string]*graphqllearning.Store
-)
-
-func FindStoreByID(id string) (*graphqllearning.Store, error) {
-
-	stores = make(map[string]*graphqllearning.Store)
-
-	stores["steam"] = &graphqllearning.Store{
+var stores = map[string]*graphqllearning.Store{
+	"steam": {
 		ID:   "steam",
 		Name: "Steam",
-	}
-
-	stores["epic"] = &graphqllearning.Store{
+	},
+	"epic": {
 		ID:   "epic",
 		Name: "Epic Games",
-	}
+	},
 
-	stores["psn"] = &graphqllearning.Store{
+	"psn": {
 		ID:   "psn",
 		Name: "Playstation Store",
-	}
+	},
+}
 
+func FindStoreByID(id string) (*graphqllearning.Store, error) {
 	if stores[id] == nil {
 		return nil, fmt.Errorf("STORE DOESN'T EXIST")
 	}
